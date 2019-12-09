8ab2cc3d270ff20a9d299488ae1a318c3df422ed
Sridevi Alawandi <sridevialawandi@gmail.com>
2019-12-09 15:33:39 +0000

    bezier foundation

---
+++ bezier_curve_algo.cpp
@@ -0,0 +1,42 @@
+//Indian Flag Animation using Bezier Curve Algorithm
+#include<GL/glut.h>
+#include<stdio.h>
+#include<math.h>
+#define PI 3.1416
+
+GLsizei winWidth = 600, winHeight = 600;
+GLfloat xwcMin = 0.0, xwcMax = 130.0;
+GLfloat ywcMin = 0.0, ywcMax = 130.0;
+
+typedef struct wcPt3D
+{
+	GLfloat x, y, z;
+};
+
+void bino(GLint n, GLint* C)
+{
+	GLint k, j;
+	for (k = 0; k <= n; k++)
+	{
+		C[k] = 1;
+		for (j = n; j >= k + 1; j--)
+			C[k] *= j;
+		for (j = n - k; j >= 2; j--)
+			C[k] /= j;
+	}
+}
+
+void computeBezPt(GLfloat u, wcPt3D* bezPt, GLint nCtrlPts, wcPt3D* ctrlPts, GLint* C)
+{
+	GLint k, n = nCtrlPts - 1;
+	GLfloat bezBlendFcn;
+	bezPt->x = bezPt->y = bezPt->z = 0.0;
+	for (k = 0; k < nCtrlPts; k++)
+	{
+		bezBlendFcn = C[k] * pow(u, k) * pow(1 - u, n - k);
+		bezPt->x += ctrlPts[k].x * bezBlendFcn;
+		bezPt->y += ctrlPts[k].y * bezBlendFcn;
+		bezPt->z += ctrlPts[k].z * bezBlendFcn;
+	}
+}
+
